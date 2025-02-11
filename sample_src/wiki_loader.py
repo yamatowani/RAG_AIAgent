1680b1bb6cb113ebbf2d4757b8ee50f202ffec17
yamatowani <festajaune.18637@icloud.com>
2025-02-11 14:50:05 +0900

    add: sample_src

---
+++ sample_src/wiki_loader.py
@@ -0,0 +1,34 @@
+import bs4
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WikipediaLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.1)
+
+loader = WikipediaLoader(query="Jリーグ", lang="ja")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+result = rag_chain.invoke("問題文と4つの選択肢からなるクイズを10問作成して")
+print(result)
